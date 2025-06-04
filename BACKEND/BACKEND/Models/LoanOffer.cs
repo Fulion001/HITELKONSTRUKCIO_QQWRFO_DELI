eefd53fa1362cf57c6c81b3e1cf1ac78e8ccbcc0
Fulion001 <114493042+Fulion001@users.noreply.github.com>
2025-06-04 08:47:16 +0200

    LoanRequest and LoanOffer structure finished

--- BACKEND/BACKEND/Models/LoanOffer.cs
+++ BACKEND/BACKEND/Models/LoanOffer.cs
@@ -2,5 +2,13 @@
 {
     public class LoanOffer
     {
+        // Futamidő években
+        public int Years { get; set; }
+
+        // Havi törlesztőrészlet
+        public double MonthlyPayment { get; set; }
+
+        // Teljes visszafizetendő összeg
+        public double TotalRepayment { get; set; }
     }
 }
--- BACKEND/BACKEND/Models/LoanRequest.cs
+++ BACKEND/BACKEND/Models/LoanRequest.cs
@@ -2,5 +2,10 @@
 {
     public class LoanRequest
     {
+        // A felvenni kívánt hitel összege
+        public double LoanAmount { get; set; }
+
+        // A megadott kamatláb (pl. 7.5)
+        public double InterestRate { get; set; }
     }
 }
